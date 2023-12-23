30a527f01782ee19df2e0c1df523e0a5127e08d6
Drportox <guiportopt@outlook.com>
2023-12-23 03:04:04 +0000

    new update menu fixed

--- assets/js/staro.js
+++ assets/js/staro.js
@@ -1,10 +1,21 @@
+// staro.js
+
 function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
-  }
-  
-  function hideMenu() {
+  var menu = document.querySelector('.menu');
+  menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+}
+
+
+
+// Verifica a largura da tela antes de executar a função
+function hideMenu() {
+  if (window.innerWidth <= 800) {
     var menu = document.querySelector('.menu');
     menu.style.display = 'none';
-    menu.classList.remove('active');
-  }
+
+  } else {
+      // Execute sua lógica para telas maiores que 600 pixels, se necessário
+      menu.style.display = 'block';
+    }
+}
+hideMenu();
