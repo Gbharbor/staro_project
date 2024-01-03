942e370b6b67dabf39e374a04c608a73d1a884e3
Drportox <guiportopt@outlook.com>
2024-01-03 20:56:36 +0000

    New Updates/Code Review

--- assets/js/staro.js
+++ assets/js/staro.js
@@ -1,20 +1,18 @@
-// staro.js
+function submitForm() {
+  alert('Responderemos em Breve!');
+  document.getElementById('contactForm').reset();
+}
 
 function toggleMenu() {
   var menu = document.querySelector('.menu');
   menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 }
 
-
-
-// Verifica a largura da tela antes de executar a função
 function hideMenu() {
   if (window.innerWidth <= 800) {
     var menu = document.querySelector('.menu');
     menu.style.display = 'none';
-
   } else {
-      // Execute sua lógica para telas maiores que 600 pixels, se necessário
       menu.style.display = 'block';
     }
 }
